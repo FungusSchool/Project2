4e793f94b39a37f2e52d8605400a5e2d578c7edf
Linus Strindin <Strindin.Linus@elev.ga.ntig.se>
2022-03-14 11:00:43 +0100

    Dice

--- dice.js
+++ dice.js
@@ -2,16 +2,24 @@
 
 
 
-// diceRoll.addEventListener("click", roll);
 let diceRoll = document.querySelector(".diceRoll")
 diceRoll.addEventListener("click",roll);
 let showdice = document.querySelector(".dice").innerHTML;
+let images = [
+    "diceSides\dice-six-faces-one.png",
+    "diceSides\dice-six-faces-two.png",
+    "diceSides\dice-six-faces-three.png",
+    "diceSides\dice-six-faces-four.png",
+    "diceSides\dice-six-faces-five.png",
+    "diceSides\dice-six-faces-six.png"
+]
 function roll(){
     let dice1 = Math.floor(Math.random()*6)+1; 
     let dice2 = Math.floor(Math.random()*6)+1; 
     console.log(dice1);
     console.log(dice2);
     showdice = (`${dice1}`);
+    document.querySelector(".dice1").setAttribute("src","diceSides\\dice-six-faces-six.png")
 }
 
 
