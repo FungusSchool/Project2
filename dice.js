173177c10df76830179cc968ebd988356ab40857
Linus Strindin <Strindin.Linus@elev.ga.ntig.se>
2022-05-09 11:00:16 +0200

    High Low

--- dice.js
+++ dice.js
@@ -1,6 +1,7 @@
+let money = 100;
 
-let diceRoll = document.querySelector(".diceRoll")
-diceRoll.addEventListener("click",roll);
+let diceRollH = document.querySelector(".diceRollH")
+diceRollH.addEventListener("click",rollH);
 let images = [
     "diceSides\\dice-six-faces-one.png",
     "diceSides\\dice-six-faces-two.png",
@@ -9,16 +10,40 @@ let images = [
     "diceSides\\dice-six-faces-five.png",
     "diceSides\\dice-six-faces-six.png"
 ]
-function roll(){
+function rollH(){
     let dice1 = Math.floor(Math.random()*6)+1; 
     let dice2 = Math.floor(Math.random()*6)+1; 
     console.log(dice1);
     console.log(dice2);
+    
     document.querySelector(".dice1").setAttribute("src",images[dice1-1])
     document.querySelector(".dice2").setAttribute("src",images[dice2-1])
     let score = dice1 + dice2;
     document.querySelector(".textBelow").innerHTML = `You got ${score} points!`
+     if (score > 7) {
+        money *= 2;
+    } else {
+        money /= 2;
+    } 
+    let moneyText = document.querySelector(".moneyText").innerHTML = `Money: ${money}$`
 }
-
-
+let diceRollL = document.querySelector(".diceRollL")
+diceRollL.addEventListener("click",rollL);
+function rollL(){
+    let dice1 = Math.floor(Math.random()*6)+1; 
+    let dice2 = Math.floor(Math.random()*6)+1; 
+    console.log(dice1);
+    console.log(dice2);
+    
+    document.querySelector(".dice1").setAttribute("src",images[dice1-1])
+    document.querySelector(".dice2").setAttribute("src",images[dice2-1])
+    let score = dice1 + dice2;
+    document.querySelector(".textBelow").innerHTML = `You got ${score} points!`
+     if (score < 7) {
+        money *= 2;
+    } else {
+        money /= 2;
+    } 
+    let moneyText = document.querySelector(".moneyText").innerHTML = `Money: ${money}$`
+}
 
